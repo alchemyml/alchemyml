ea779f5dc652d85a1b8afbbbe9372973d11f9f12
ahuizi <ahuizi@skootik.com>
2020-03-18 08:17:24 +0100

    AHZI_Adds methods and information related with version

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 import setuptools
 
+import re
+VERSIONFILE = "alchemyml/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
 with open('README.md', 'r') as file:
     long_description = file.read()
 
 setuptools.setup(
     name='alchemyml',  
-    version='0.1.2',
+    version=verstr,
     author="Alchemy Machine Learning, S. L.",
     author_email="admin@alchemyml.com",
     description="AlchemyML API package",

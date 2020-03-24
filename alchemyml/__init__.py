a3c40f5c07b2733cc25cb3e15500c0d2db98726d
ahuizi <ahuizi@skootik.com>
2020-03-24 14:40:33 +0100

    AHZI_Updates __init__.py file

--- alchemyml/__init__.py
+++ alchemyml/__init__.py
@@ -7,15 +7,11 @@ __doc__ = '''
 alchemyml - Automation of data lifecycle
 ========================================
 
-**alchemyml** is a tool for automating the data lifecycle and accelerating 
-advanced data analysis processes. 
-
-AlchemyML es una herramienta para automatizar el ciclo de vida del dato y 
-acelerar procesos de analítica avanzada de datos. Partiendo de la ingesta y 
-preparación del dato, pasando por el preprocesamiento y hasta llegar a la 
-construcción de modelos predictivos, AlchemyML provee al científico de datos 
-de herramientas para acelerar su 
-
+**alchemyml** is a multi-environment tool for automating the data lifecycle 
+and accelerating advanced data analysis processes. From data ingest and 
+preparation, through pre-processing and up to the construction of predictive 
+models, **alchemyml** provides the data scientist with aldata science applied 
+artificial intelligence tools to optimize their work, time and resources. 
 '''
 
 
